c77bf2b8092a6672347f5dc0c14292ed359201ff
Sikander-Ali-Bhatti <152159856+Sikander-Ali-Bhatti@users.noreply.github.com>
2024-01-12 22:20:00 +0500

    Add files via upload

---
+++ problem1 .py
@@ -0,0 +1,22 @@
+#part(i)
+list=[1,2,3]
+my_tuple=tuple(list)
+print(my_tuple)
+
+#part(ii)
+student_info={'name':'alice','age':'20:','grade':'A'}
+print(student_info['age'])
+
+#part(iii)
+def square(x):
+    return x**2
+result=square
+print(result)
+
+
+
+#part(iv)
+num1=7
+num2=3
+result=num1%num2 
+print(result)
---
+++ problem2.py
@@ -0,0 +1,19 @@
+#part(i)
+value=2**3*2
+print(value)
+
+#part(ii)
+student_info={'name':'alice','age':'20:','grade':'A'}
+print(student_info['age'])
+
+#part(iii)
+x=15
+if x>10 and x%2==1:
+    print("odd and greater than 10")
+else:
+    print("not meeting critieria")    
+    
+    
+#part(iv)
+for i in range (2,8,2) :
+    print(i)   
